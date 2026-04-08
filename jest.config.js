3a4979dacfc04eb54d00169cfbdd6a20e59d966e
Katlego-Bruce <85530704+Katlego-Bruce@users.noreply.github.com>
2026-04-08 14:09:22 +0200

    chore: add Jest configuration
    Ref #10

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterSetup: ['./tests/setup.js'],
+  testTimeout: 30000,
+  verbose: true,
+  forceExit: true,
+  detectOpenHandles: true,
+};
