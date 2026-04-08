494219edaa2125f9d821b69538916e78454bed27
cece80 <ramokgadi.ks@gmail.com>
2026-04-08 08:59:14 +0200

    feat: runtime roles - update auth.controller.js

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -2,14 +2,13 @@ const authService = require('./auth.service');
 const asyncHandler = require('../../utils/asyncHandler');
 
 exports.register = asyncHandler(async (req, res) => {
-  const { firstName, lastName, phone, password, role, skills } = req.body;
+  const { firstName, lastName, phone, password, skills } = req.body;
 
   const result = await authService.register({
     firstName,
     lastName,
     phone,
     password,
-    role,
     skills: skills || [],
   });
 
@@ -21,17 +20,10 @@ exports.register = asyncHandler(async (req, res) => {
 
 exports.login = asyncHandler(async (req, res) => {
   const { phone, password } = req.body;
-
   const result = await authService.login({ phone, password });
-
-  res.json({
-    message: 'Login successful',
-    data: result,
-  });
+  res.json({ message: 'Login successful', data: result });
 });
 
 exports.getMe = asyncHandler(async (req, res) => {
-  res.json({
-    data: req.user,
-  });
+  res.json({ data: req.user });
 });
